adae00e9558a1b9773ed618b22d869151d1d9651
Meet Thaker <thakermeet@yahoo.in>
2025-05-25 12:41:28 +0530

    Add files via upload

---
+++ evenodd.py
@@ -0,0 +1,10 @@
+num = input("Enter the Number:")
+
+num = int(num)
+
+if num%2==0:
+    print(num," is an even number.")
+else:
+    print(num," is an odd number.")
+
+
---
+++ sum.py
@@ -0,0 +1,6 @@
+database = list(range(1,51))
+total = 0
+for i in database:
+    total = total +i
+
+print("the SUM of the number 1 to 50 is:",total)
